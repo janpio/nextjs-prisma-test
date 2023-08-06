6a13148f5b202dc48f1bfd817c888f78d825da3d
Jan Piotrowski <piotrowski+github@gmail.com>
2023-08-06 13:02:18 +0000

    remove more stuff

--- pages/admin/dictionary/reference.tsx
+++ pages/admin/dictionary/reference.tsx
@@ -18,7 +18,7 @@ import React, { FormEvent, useState } from 'react';
 type Props = {};
 
 export default function Reference({}: Props) {
-  const { data: session, status } = useSession();
+  // const { data: session, status } = useSession();
   const router = useRouter();
   const {
     data: dictionaryData,
@@ -106,97 +106,8 @@ export default function Reference({}: Props) {
 
   return (
     <>
-      {session && (
-        <>
-          {session.user?.email ? (
-            <>
-              <br />
-              <br />
-              <div className='px-5 text-xs'>
-                <form onSubmit={handleSubmitDeleteDictionaryItem}>
-                  <div className='flex flex-row'>
-                    [{+router.query.id!}] {DictionaryItem.name}
-                    <button type='submit' className='ml-2'>
-                      {/* <MinusCircleSVG /> */}
-                    </button>
-                  </div>
-                </form>
-
-                {DictionaryItem.name && (
-                  <>
-                    <form onSubmit={handleSubmitUpdateDictionaryItemName}>
-                      <div className='flex'>
-                        <input
-                          type='text'
-                          placeholder='Modificare cuvânt ...'
-                          className='mx-2 border-2 border-indian-khaki-400 dark:border-indian-khaki-200 rounded'
-                          value={updatedName}
-                          onChange={(e) => setUpdatedName(e.target.value)}
-                        />
-                        <button type='submit' disabled={!updatedName}>
-                          {/* <CheckCircleSVG */}
-                            {/* color={updatedName ? '#766852' : '#D6C6AC'} */}
-                          {/* /> */}
-                        </button>
-                      </div>
-                    </form>
-
-                    <div className='mt-4'>
-                      {DictionaryItem.translation}
-                      <form
-                        onSubmit={handleSubmitUpdateDictionaryItemTranslation}
-                      >
-                        <div className='flex'>
-                          <input
-                            type='text'
-                            placeholder='Modificare traducere ...'
-                            className='mx-2 my-1 border-2 w-full border-indian-khaki-400 dark:border-indian-khaki-200 rounded'
-                            value={updatedTranslation}
-                            onChange={(e) =>
-                              setUpdatedTranslation(e.target.value)
-                            }
-                          />
-                          <button type='submit' disabled={!updatedTranslation}>
-                            {/* <CheckCircleSVG
-                              color={updatedTranslation ? '#766852' : '#D6C6AC'}
-                            /> */}
-                          </button>
-                        </div>
-                      </form>
-                    </div>
-
-                    <div className='mt-4'>
-                      {DictionaryItem.explanation}
-                      <form
-                        onSubmit={handleSubmitUpdateDictionaryItemExplanation}
-                      >
-                        <div className='flex'>
-                          <input
-                            type='text'
-                            placeholder='Modificare explicații ...'
-                            className='mx-2 my-1 border-2 w-full border-indian-khaki-400 dark:border-indian-khaki-200 rounded'
-                            value={updatedExplanation}
-                            onChange={(e) =>
-                              setUpdatedExplanation(e.target.value)
-                            }
-                          />
-                          <button type='submit' disabled={!updatedExplanation}>
-                            {/* <CheckCircleSVG
-                              color={updatedExplanation ? '#766852' : '#D6C6AC'}
-                            /> */}
-                          </button>
-                        </div>
-                      </form>
-                    </div>
-                  </>
-                )}
-              </div>
-            </>
-          ) : (
-            <DoNotEnter />
-          )}
-        </>
-      )}
+      {
+      }
     </>
   );
 }
